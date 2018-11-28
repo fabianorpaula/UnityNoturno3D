d69481be98568c1a73cead7ab0bb25ff8f05adbf
@fabiano.paula <fabiano.rpaula@gmail.com>
2018-11-27 23:25:23 -0200

    #camera

---
+++ Assets/Scripts/CameraSeguir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguir : MonoBehaviour {
+    public GameObject Personagem;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(Personagem.transform.position.x, 5, Personagem.transform.position.z-5);
+        
+	}
+}
